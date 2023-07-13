ee5ded2dc828e336d50bd2b27f9b5f35e388be31
Pedro <pedrovargastannuri@gmail.com>
2023-07-13 11:56:29 -0300

    acho que vou parar de mexer nisso por enquanto

--- Arvore.hs
+++ Arvore.hs
@@ -1,4 +1,5 @@
 module Tree where
+
 data BinTree a = Nil | Node a (BinTree a) (BinTree a)
     deriving (Show, Eq, Ord)
 
@@ -14,7 +15,7 @@ instance Applicative BinTree where
     (<*>) :: BinTree (a -> b) -> BinTree a -> BinTree b
     Nil <*> _ = Nil
     _ <*> Nil = Nil
-    (Node fa fl fr) <*> (Node x y z) = (Node (fa x) (fl <*> y) (fr <*> z))
+    (Node fa fl fr) <*> (Node x y z) = Node (fa x) (fl <*> y) (fr <*> z)
 
 --instance Monad BinTree where
 --    Nil >>= f = Nil
@@ -26,24 +27,24 @@ instance Applicative BinTree where
 preorder :: BinTree a -> [a]
 
 preorder Nil = []
-preorder (Node x l r) = x : (preorder l) ++ (preorder r)
+preorder (Node x l r) = x : preorder l ++ preorder r
 
 inorder :: BinTree a -> [a]
 
 inorder Nil = []
-inorder (Node x l r) = (inorder l) ++ [x] ++ (inorder r)
+inorder (Node x l r) = inorder l ++ [x] ++ inorder r
 
 postorder :: BinTree a -> [a]
 
 postorder Nil = []
-postorder (Node x l r) = (postorder l) ++ (postorder r) ++ [x]
+postorder (Node x l r) = postorder l ++ postorder r ++ [x]
 
 --
 --calculaAltura' :: (Ord a, Ord b, Num b) => BinTree (a,b) -> b -> b
 --calculaAltura' Nil count = count
 --calculaAltura' (Node x l r) count = count + (max (calculaAltura' l 1) (calculaAltura' r 1))
 --
-    --calculaAltura :: (Ord a,Ord b, Num b) => BinTree (a,b) -> b
+--calculaAltura :: (Ord a,Ord b, Num b) => BinTree (a,b) -> b
 --calculaAltura (Node (v,h) l r) = calculaAltura' (Node (v,h) l r) h
 --
 --
@@ -56,40 +57,41 @@ postorder (Node x l r) = (postorder l) ++ (postorder r) ++ [x]
 
 
 insertElem :: Ord a => BinTree a -> a -> BinTree a
-insertElem Nil e = (Node e) Nil Nil
+insertElem Nil e = Node e Nil Nil
 insertElem (Node x l r) e
-    |e > x = (Node x l (insertElem r e))
-    |e < x = (Node x (insertElem l e) r)
-    |otherwise = (Node x l r) 
+    |e > x = Node x l (insertElem r e)
+    |e < x = Node x (insertElem l e) r
+    |otherwise = Node x l r 
 
 insertSubarvore :: Ord a => BinTree a -> BinTree a -> BinTree a
 
 insertSubarvore Nil Nil  = Nil
-insertSubarvore Nil (Node x1 l1 r1)  = (Node x1 l1 r1) 
-insertSubarvore (Node x l r) Nil = (Node x l r)
+insertSubarvore Nil (Node x1 l1 r1)  = Node x1 l1 r1 
+insertSubarvore (Node x l r) Nil = Node x l r
 insertSubarvore (Node x l r) (Node x1 l1 r1) 
-    |x == x1 = (Node x (insertSubarvore l l1) (insertSubarvore r r1))
-    |x < x1 = (Node x l (insertSubarvore r (Node x1 l1 r1)))
-    |x > x1 = (Node x (insertSubarvore l (Node x1 l1 r1)) r)
+    |x == x1 = Node x (insertSubarvore l l1) (insertSubarvore r r1)
+    |x < x1 = Node x l (insertSubarvore r (Node x1 l1 r1))
+    |x > x1 = Node x (insertSubarvore l (Node x1 l1 r1)) r
 
 removeSubBinTree :: Ord a => BinTree a -> a -> BinTree a
 removeSubBinTree Nil x = Nil
 removeSubBinTree (Node x l r) e
     |e == x = Nil
-    |e > x = (Node x l (removeSubBinTree r e))
-    |e < x = (Node x (removeSubBinTree l e) r)
+    |e > x = Node x l (removeSubBinTree r e)
+    |e < x = Node x (removeSubBinTree l e) r
 
 removeElem' :: Ord a => Eq b => Num b => BinTree a -> a -> b -> BinTree a
 removeElem' Nil x _= Nil
 removeElem' (Node x l r) e before
     |e == x && before == 1 = insertSubarvore l r
     |e == x && before == -1 = insertSubarvore r l
-    |e > x = (Node x l (removeElem' r e 1))
-    |e < x = (Node x (removeElem' l e (-1)) r)
+    |e > x = Node x l (removeElem' r e 1)
+    |e < x = Node x (removeElem' l e (-1)) r
 
 removeElem :: Ord a => BinTree a -> a -> BinTree a
 removeElem arv e = removeElem' arv e 1
 
-tree1 = Node 5 (Node 4 (Node 3 Nil (Node 2 Nil Nil) ) (Nil)) (Node 6 Nil (Node 9 (Node 7 Nil Nil) (Node 10 Nil Nil)))
-tree2 = Node 5 (Node 4 (Node 3 Nil (Node 2 Nil Nil) ) (Nil)) (Node 6 Nil (Node 9 (Node 7 Nil Nil) (Node 10 Nil Nil)))
+tree1 = Node 5 (Node 4 (Node 3 Nil (Node 2 Nil Nil) ) Nil) (Node 6 Nil (Node 9 (Node 7 Nil Nil) (Node 10 Nil Nil)))
+tree2 = (+) 1 <$> tree1 --Sums one to every single element of tree
+tree3 = (+) <$> tree1 <*> tree2 -- very cool shit we're able to do cuz of Applicative
 
