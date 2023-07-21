fc9dc85db24a9bf312d9e3726dde4533bba5f38c
Pedro <pedrovargastannuri@gmail.com>
2023-07-21 07:24:42 -0300

    a

--- generic_mergesort.c
+++ generic_mergesort.c
@@ -6,18 +6,18 @@ void mg(void *vet, int s, int mid, int e, size_t typeSize, int (*compare)(const 
 {
     int len1 = mid - s + 1;
     int len2 = e - mid;
-    // Using char* to be able to manipulate elements from the vector (that now is void*).
-    // We use memcpy and pointer arith to copy elements from the vector.
-    // The use of char* fits nicely because char is one byte long, so if we're working, for example, with int values.
-    // We know we must increment the array by 4 bytes to get to the next position.
-    // That's why we use char*, so that arr += 4 increments the address in four bytes.
-    // OBS: We could probably use our void* to perform these operations, but it isn't complient with C Standard,
-    // the Standard is to use char* to perform these operations.
+    /* Using char* to be able to manipulate elements from the vector (that now is void*).
+       We use memcpy and pointer arith to copy elements from the vector.
+       The use of char* fits nicely because char is one byte long, so if we're working, for example, with int values.
+       We know we must increment the array by 4 bytes to get to the next position.
+       That's why we use char*, so that arr += 4 increments the address in four bytes.
+       OBS: We could probably use our void* to perform these operations, but it isn't complient with C Standard,
+       the Standard is to use char* to perform these operations. */
     char *arr = (char *)vet;
     char arr1[len1 * typeSize];
     char arr2[len2 * typeSize];
 
-    // Constructing the aux arrays.
+    /* Constructing the aux arrays. */
     for (int x = 0; x < len1; x++)
     {
         memcpy(arr1 + x * typeSize, arr + (s + x) * typeSize, typeSize);
@@ -34,7 +34,7 @@ void mg(void *vet, int s, int mid, int e, size_t typeSize, int (*compare)(const 
 
     while (i < len1 && j < len2)
     {
-        // We use these void ptrs to make use of our compare function
+        /* We use these void ptrs to make use of our compare function*/
         void *elem1;
         void *elem2;
         elem1 = (void *)(arr1 + i * typeSize);
@@ -82,9 +82,9 @@ void ms(void *vet, int s, int e, size_t typeSize,int (*compare)(const void*, con
 
 int cmp(const void *a, const void *b)
 {
-    // int compare function
-    // For this generic mergesort algorithm, we must implement a function that returns 1 or 0
-    // I didn't figure out how to use the "real" cmp function, i.e, the one that returns void*
+    /* int compare function
+       For this generic mergesort algorithm, we must implement a function that returns 1 or 0
+       I didn't figure out how to use the "real" cmp function, i.e, the one that returns void* */
     if (*(int *)a <= *(int *)b)
         return 1;
     return 0;
@@ -94,8 +94,6 @@ int main()
 {
     int vet[] = {34234, 1, 0, 9, 7, 4, 6, 6, 6};
 
-    int x;
-    int y;
-    ms(vet, 0, 8, sizeof(int), cmp);
+    ms((void*)vet, 0, 8, sizeof(int), cmp);
     return 0;
 }
